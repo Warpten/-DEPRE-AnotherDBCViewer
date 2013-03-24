f5e5fcc5d282b0b0c7d5a2566d13f3f411cd69c7
Kinzcool <kinzzcool@hotmail.com>
2013-03-23 21:52:43 -0400

    Added Item.db2 & ItemExtendedCost.db2 definitions

---
+++ ClientDefinitions/DB2/Cataclysm/ItemEntry.cs
@@ -0,0 +1,16 @@
+﻿using DBFilesClient.NET;
+
+namespace FileStructures.DB2.Cataclysm
+{
+    public sealed class ItemEntry : BaseDbcFormat
+    {
+        public uint ID;
+        public uint Class;
+        public uint SubClass;
+        public int SoundOverrideSubclass;
+        public int Material;
+        public uint DisplayId;
+        public uint InventoryType;
+        public uint Sheath;
+    }
+}
---
+++ ClientDefinitions/DB2/Cataclysm/ItemExtendedCostEntry.cs
@@ -0,0 +1,27 @@
+﻿using DBFilesClient.NET;
+
+namespace FileStructures.DB2.Cataclysm
+{
+    public sealed class ItemExtendedCostEntry : BaseDbcFormat
+    {
+        public uint ID;
+        public uint RequiredHonorPoints;
+        public uint RequiredArenaPoints;
+        public uint RequiredArenaSlot;
+        [StoragePresence(StoragePresenceOption.Include, ArraySize = 5)]
+        public uint[] RequiredItem;
+        [StoragePresence(StoragePresenceOption.Include, ArraySize = 5)]
+        public uint[] RequiredItemCount;
+        public uint RequiredPersonalArenaRating;
+        public uint ItemPurchaseGroup;
+        [StoragePresence(StoragePresenceOption.Include, ArraySize = 5)]
+        public uint[] RequiredCurrency;
+        [StoragePresence(StoragePresenceOption.Include, ArraySize = 5)]
+        public uint[] RequiredCurrencyCount;
+        public uint Unk1;
+        public uint Unk2;
+        public uint Unk3;
+        public uint Unk4;
+        public uint Unk5;
+    }
+}
